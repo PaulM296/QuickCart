c58d54b09a285e4491adb75809786b50fe82b3af
PaulM <paulmicluta@gmail.com>
2024-10-15 12:14:50 +0300

    Add project files.

---
+++ QuickCart.Infrastructure/Configurations/ProductConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuickCart.Domain.Entities;
+
+namespace QuickCart.Infrastructure.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder
+                .Property(x => x.Price)
+                .HasColumnType("decimal(18,2)");
+            builder
+                .Property(x => x.Name).IsRequired();
+        }
+    }
+}
---
+++ QuickCart.Infrastructure/Data/QuickCartDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuickCart.Domain.Entities;
+using QuickCart.Infrastructure.Configurations;
+
+namespace QuickCart.Infrastructure.Data
+{
+    public class QuickCartDbContext(DbContextOptions options) : DbContext(options)
+    {    
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ProductConfiguration).Assembly);
+        }
+    }
+}
