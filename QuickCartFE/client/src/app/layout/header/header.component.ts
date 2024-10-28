8c64945051d1b2e09bc8ac1f63658edcbb412750
PaulM <paulmicluta@gmail.com>
2024-10-28 19:50:09 +0200

    Implemented adding an item to cart functionality, persisted the cart and updated the navbar with the cart item

--- QuickCartFE/client/src/app/layout/header/header.component.ts
+++ QuickCartFE/client/src/app/layout/header/header.component.ts
@@ -5,6 +5,8 @@ import { MatIcon } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { BusyService } from '../../core/services/busy.service';
 import { MatProgressBar } from '@angular/material/progress-bar';
+import { Cart } from '../../shared/models/cart';
+import { CartService } from '../../core/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -22,4 +24,5 @@ import { MatProgressBar } from '@angular/material/progress-bar';
 })
 export class HeaderComponent {
   busyService = inject(BusyService);
+  cartService = inject(CartService);
 }
