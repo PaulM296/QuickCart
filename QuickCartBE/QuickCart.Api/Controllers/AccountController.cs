854fd0701933c3299486063aa0945a80fdf1d3b9
PaulM <paulmicluta@gmail.com>
2024-10-29 11:24:41 +0200

    Added endpoint in order to update the user address

--- QuickCartBE/QuickCart.Api/Controllers/AccountController.cs
+++ QuickCartBE/QuickCart.Api/Controllers/AccountController.cs
@@ -50,13 +50,14 @@ namespace QuickCart.Api.Controllers
             if (User.Identity?.IsAuthenticated == false)
                 return NoContent();
 
-            var user = await signInManager.UserManager.GetUserByEmail(User);
+            var user = await signInManager.UserManager.GetUserByEmailWithAddress(User);
 
             return Ok(new
             {
                 user.FirstName,
                 user.LastName,
-                user.Email
+                user.Email,
+                Address = user.Address?.ToDto()
             });
         }
 
@@ -67,6 +68,29 @@ namespace QuickCart.Api.Controllers
             {
                 IsAuthenticated = User.Identity?.IsAuthenticated ?? false
             });
+        }
+
+        [Authorize]
+        [HttpPost("address")]
+        public async Task<ActionResult<Address>> CreateOrUpdateAddress(AddressDto addressDto)
+        {
+            var user = await signInManager.UserManager.GetUserByEmailWithAddress(User);
+
+            if(user.Address == null)
+            {
+                user.Address = addressDto.ToEntity();
+            }
+            else
+            {
+                user.Address.UpdateFromDto(addressDto);
+            }
+
+            var result = await signInManager.UserManager.UpdateAsync(user);
+        
+            if(!result.Succeeded)
+                return BadRequest("Problem updating user address");
+
+            return Ok(user.Address.ToDto());
         }
     }
 }
