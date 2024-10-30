23ea97f0412ba9cc693d860559fe0ed87ff6a3f8
PaulM <paulmicluta@gmail.com>
2024-10-30 15:03:06 +0200

    Added Stripe on the client side and created the address element

--- QuickCartFE/client/src/environments/environment.development.ts
+++ QuickCartFE/client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7226/api/'
+    apiUrl: 'https://localhost:7226/api/',
+    stripePublicKey: 'pk_test_51QFXABGTeJD08wNz9ggK8TcojRT00GkjF0W5mAwaFd0pOcxOcAvKlTmWCM6DlnppcDJexKMDj7KA9FQElHuB9gGG0098DEnJJ8'
 };
--- QuickCartFE/client/src/environments/environment.ts
+++ QuickCartFE/client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51QFXABGTeJD08wNz9ggK8TcojRT00GkjF0W5mAwaFd0pOcxOcAvKlTmWCM6DlnppcDJexKMDj7KA9FQElHuB9gGG0098DEnJJ8'
 };
