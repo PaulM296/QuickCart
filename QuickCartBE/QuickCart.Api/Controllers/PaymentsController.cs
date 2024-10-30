55e645c4cf443ac5f9d54c4715f7ba84f79f4477
PaulM <paulmicluta@gmail.com>
2024-10-30 12:05:23 +0200

    Created PaymentController

---
+++ QuickCartBE/QuickCart.Api/Controllers/PaymentsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using QuickCart.Domain.Entities;
+using QuickCart.Domain.Interfaces;
+
+namespace QuickCart.Api.Controllers
+{
+    public class PaymentsController(IPaymentService paymentService,
+        IBaseRepository<DeliveryMethod> dmRepo) : BaseApiController
+    {
+        [Authorize]
+        [HttpPost("{cartId}")]
+        public async Task<ActionResult<ShoppingCart>> CreateOrUpdatePaymentIntent(string cartId)
+        {
+            var cart = await paymentService.CreateOrUpdatePaymentIntent(cartId);
+
+            if (cart == null)
+                return BadRequest("Problem with your cart.");
+
+            return Ok(cart);
+        }
+
+        [HttpGet("delivery-methods")]
+        public async Task<ActionResult<IReadOnlyList<DeliveryMethod>>> GetDeliveryMethods()
+        {
+            return Ok(await dmRepo.ListAllAsync());
+        }
+    }
+}
