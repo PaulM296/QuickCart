9b3b12a07164469a0463e731712cabc7dbd52de2
PaulM <paulmicluta@gmail.com>
2024-10-30 14:27:51 +0200

    Checkout page layout resolved

--- QuickCartFE/client/src/app/features/checkout/checkout.component.ts
+++ QuickCartFE/client/src/app/features/checkout/checkout.component.ts
@@ -1,12 +1,14 @@
-import { Component, inject } from '@angular/core';
-import { AccountService } from '../../core/services/account.service';
-import { Router } from '@angular/router';
-import { CartService } from '../../core/services/cart.service';
+import { Component } from '@angular/core';
+import { MatStepperModule } from '@angular/material/stepper';
+import { OrderSummaryComponent } from "../../shared/components/order-summary/order-summary.component";
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [
+    OrderSummaryComponent,
+    MatStepperModule
+  ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
 })
